eeb86fd03dac5a1822d244a64a8b0b168b635c08
John Matter <matter@virginia.edu>
2017-12-19 17:23:20 -0500

    Check if raw data exist; use full path for batch script

--- hcswif.py
+++ hcswif.py
@@ -9,15 +9,18 @@ import datetime
 #------------------------------------------------------------------------------
 # Preliminary workspace stuff
 
-# if work_dir doesn't exist, exit and warn user
+# Where do you want the output of your analyses?
 work_dir = os.path.join('/volatile/hallc/comm2017/', getpass.getuser())
 if not os.path.isdir(work_dir):
     raise StandardError('work_dir: ' + work_dir + ' does not exist')
 
-# if raw_dir doesn't exist, exit and warn user
+# Where is your raw data?
 raw_dir = '/mss/hallc/spring17/raw'
 if not os.path.isdir(raw_dir):
     raise StandardError('raw_dir: ' + raw_dir + ' does not exist')
+
+# Where is hcswif?
+hcswif_dir = os.path.dirname(os.path.realpath(__file__))
 
 # hcswif_prefix is used as prefix for workflow, job names, filenames, etc.
 now = datetime.datetime.now()
@@ -36,7 +39,7 @@ parser.add_argument('spectrometer', nargs=1,
                     help='spectrometer to analyze (HMS, SHMS, COIN)')
 parser.add_argument('--run', nargs='+', dest='run', 
                     help='a space-separated list of run number(s)')
-parser.add_argument('--events', nargs=1, dest='events', default=50000,
+parser.add_argument('--events', nargs=1, dest='events',
                     help='number of events to analyze (default=50k)')
 parser.add_argument('--outfile', nargs=1, dest='outfile', 
                     help='name of output json file')
@@ -53,8 +56,11 @@ if spectrometer.upper() not in ['HMS','SHMS','COIN']:
     raise ValueError('Spectrometer must be HMS, SHMS, or COIN')
 
 # Number of events
-evts = parsed_args.events[0]
-    
+if parsed_args.events==None:
+    evts = 50000
+else:
+    evts = parsed_args.events[0]
+
 # Run(s)
 if parsed_args.run==None:
     raise StandardError('Must specify run(s) to process')
@@ -64,7 +70,7 @@ else:
 # Outfile
 if parsed_args.outfile==None:
     outfile = hcswif_prefix + '.json'
-    outfile = os.path.form(work_dir, outfile)
+    outfile = os.path.join(work_dir, outfile)
 else:
     outfile = parsed_args.outfile[0]
 
@@ -82,7 +88,7 @@ workflow = {}
 workflow['name'] = hcswif_prefix
 
 # Choose correct batch for config (e.g. hcswif_shms.sh)
-batch = 'hcswif_' + spectrometer.lower() + '.sh'
+batch = os.path.join(hcswif_dir, 'hcswif_' + spectrometer.lower() + '.sh')
 
 # Load template json for one job, then create list of jobs
 with open('job_template.json', 'r') as f:
@@ -97,6 +103,10 @@ for run in runs:
     # Assume coda name e.g. shms_all_01634
     coda_stem = spectrometer.lower() + '_all_' + str(run).zfill(5)
     coda = os.path.join(raw_dir, coda_stem + '.dat')
+
+    # Check if raw data file exist
+    if not os.path.isfile(coda):
+        raise StandardError('RAW DATA: ' + coda + ' does not exist')
 
     # Fill various fields; some are optional!
     job['project'] = project
