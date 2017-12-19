15a262496ede1cfe440597ab098779757c67ee45
John Matter <matter@virginia.edu>
2017-12-19 16:36:41 -0500

    Add ability to specify number of events, output filename, and project

--- hcswif.py
+++ hcswif.py
@@ -6,72 +6,107 @@ import getpass
 import argparse
 import datetime
 
-#----------------
-# Setup argparser
-parser = argparse.ArgumentParser(usage='%(prog)s SPECTROMETER --run R1 R2 ...')
+#------------------------------------------------------------------------------
+# Preliminary workspace stuff
 
+# if work_dir doesn't exist, exit and warn user
+work_dir = os.path.join('/volatile/hallc/comm2017/', getpass.getuser())
+if not os.path.isdir(work_dir):
+    raise StandardError('work_dir: ' + work_dir + ' does not exist')
+
+# if raw_dir doesn't exist, exit and warn user
+raw_dir = '/mss/hallc/spring17/raw'
+if not os.path.isdir(raw_dir):
+    raise StandardError('raw_dir: ' + raw_dir + ' does not exist')
+
+# hcswif_prefix is used as prefix for workflow, job names, filenames, etc.
+now = datetime.datetime.now()
+datestr = now.strftime("%Y%m%d%H%M")
+hcswif_prefix = 'hcswif' + datestr 
+
+#------------------------------------------------------------------------------
+# Parse command line arguments
+parser = argparse.ArgumentParser(usage='python hcswif.py (hms | shms | coin)' +
+                                       '--run <list of runs>' +
+                                       '--events <number of events>' +
+                                       '--outfile <output json>')
+
+# Add arguments
 parser.add_argument('spectrometer', nargs=1, 
                     help='spectrometer to analyze (HMS, SHMS, COIN)')
 parser.add_argument('--run', nargs='+', dest='run', 
                     help='a space-separated list of run number(s)')
+parser.add_argument('--events', nargs=1, dest='events', default=50000,
+                    help='number of events to analyze (default=50k)')
+parser.add_argument('--outfile', nargs=1, dest='outfile', 
+                    help='name of output json file')
+parser.add_argument('--project', nargs=1, dest='project', 
+                    help='name of project')
 
+# Parse arguments
 parsed_args = parser.parse_args()
 
-# datestr is used to name files, jobs, etc.
-now = datetime.datetime.now()
-datestr = now.strftime("%Y%m%d%H%M")
-
-# workdir should be a user's or an experiment's
-work_dir = os.path.join('/volatile/hallc/comm2017/', getpass.getuser())
-
-# if work_dir doesn't exist, exit and warn user
-if not os.path.isdir(work_dir):
-    raise StandardError('work_dir: ' + work_dir + ' does not exist')
-
-#----------------
-# Check if spectrometer is valid
+# Check validity of arguments, then assign 
+# Spectrometer
 spectrometer = parsed_args.spectrometer[0]
 if spectrometer.upper() not in ['HMS','SHMS','COIN']:
     raise ValueError('Spectrometer must be HMS, SHMS, or COIN')
 
-#----------------
-# hcswifdate is used as prefix for workflow, job names, filenames, etc.
-hcswifdate = 'hcswif' + datestr 
+# Number of events
+evts = parsed_args.events[0]
+    
+# Run(s)
+if parsed_args.run==None:
+    raise StandardError('Must specify run(s) to process')
+else:
+    runs = parsed_args.run
 
-#----------------
+# Outfile
+if parsed_args.outfile==None:
+    outfile = hcswif_prefix + '.json'
+    outfile = os.path.form(work_dir, outfile)
+else:
+    outfile = parsed_args.outfile[0]
+
+# Project
+if parsed_args.project==None:
+    raise StandardError('Must specify a project')
+else:
+    project = parsed_args.project[0]
+
+#------------------------------------------------------------------------------
+# Create JSON
+
 # Initialize workflow JSON data
 workflow = {}
-workflow['name'] = hcswifdate
+workflow['name'] = hcswif_prefix
 
-#----------------
 # Choose correct batch for config (e.g. hcswif_shms.sh)
 batch = 'hcswif_' + spectrometer.lower() + '.sh'
 
-#----------------
 # Load template json for one job, then create list of jobs
 with open('job_template.json', 'r') as f:
     job_template = json.load(f)
 
-
-# Initialize dictionary whose key is the run number, and value is a run's JSON data
+# Initialize dictionary whose key is the run number, value is a run's JSON data
 jobs = {}
-runs = parsed_args.run  # runs specified by user
 for run in runs:
-    # Initialize
+    # Initialize JSON
     job = copy.deepcopy(job_template) 
 
-    # e.g. shms_all_01634
+    # Assume coda name e.g. shms_all_01634
     coda_stem = spectrometer.lower() + '_all_' + str(run).zfill(5)
-    coda = os.path.join('/mss/hallc/spring17/raw', coda_stem + '.dat')
+    coda = os.path.join(raw_dir, coda_stem + '.dat')
 
-    job['project'] = '?????????' # scicomp website doesn't currently have a list
+    # Fill various fields; some are optional!
+    job['project'] = project
     job['email'] = getpass.getuser() + '@jlab.org'
     job['name'] = workflow['name'] + '_' + coda_stem
 
     job['stdout'] = os.path.join(work_dir, job['name'] + '.out')
     job['stderr'] = os.path.join(work_dir, job['name'] + '.err')
 
-    job['command'] = batch
+    job['command'] = " ".join([batch, str(run), str(evts)])
 
     job['track'] = 'analysis'
     job['shell'] = '/bin/bash'
@@ -92,10 +127,12 @@ for run in runs:
 
     jobs[run] = copy.deepcopy(job)
 
-# Puts jobs' JSON data into the workflow
+# Put jobs' JSON data into the workflow
 workflow['jobs'] = jobs.values()
 
-#----------------
+#------------------------------------------------------------------------------
 # Export JSON
-with open(workflow['name'] + '.json', 'w') as f:
+with open(outfile, 'w') as f:
     json.dump(workflow, f)
+
+print 'Wrote: ' + outfile
