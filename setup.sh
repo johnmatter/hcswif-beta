4498325ea12188d4e38dc4f87f3fffa71399c8e5
John Matter <matter@virginia.edu>
2017-12-20 17:12:04 -0500

    Add SHMS bash replay script draft

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# -----------------------------------------------------------------------------
+#  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+#  !!!       CHANGE ME IF THIS IS NOT WHERE hallc_replay AND hcana live   !!!
+#  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+export hcana_dir=/volatile/hallc/comm2017/$USER/hcana
+export hallc_replay_dir=/volatile/hallc/comm2017/$USER/hallc_replay
+# -----------------------------------------------------------------------------
+
+# Source setup scripts
+curdir=`pwd`
+cd $hcana_dir
+source setup.sh
+cd $hallc_replay_dir
+source setup.sh
+cd $curdir
+export PATH=$halllc_replay_dir:$hcana_dir:$PATH
